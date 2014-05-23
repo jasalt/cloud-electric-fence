0e3ad53ffc133ef7f30289f7ad6523ff97c56461
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-05-23 14:07:41 +0300

    Add gitignore. Load secrets.

--- web-client/app.js
+++ web-client/app.js
@@ -2,24 +2,34 @@
 
 /* Inject modules and set up url routings */
 angular.module("myApp", ['ngRoute'])
-.config(['$routeProvider',
-         function($routeProvider) {
+  .config(['$routeProvider',
+           function($routeProvider) {
              $routeProvider
-             .when('/', {
+               .when('/', {
                  templateUrl: 'ui.html',
                  controller: 'UiController'
-             })
+               })
              /* Other urls redirect to main page */
-             .otherwise({
+               .otherwise({
                  redirectTo: '/'
-             });
-         }
-        ])
-.controller('UiController', function ($scope) {
-
+               });
+           }
+          ])
+  .controller('UiController', function ($scope, $http) {
+    console.log("token is " + secret_access_token);
     $scope.state = true;
 
     $scope.toggle = function() {
-        $scope.state = !$scope.state;
-    }
-});
+      $http({method: 'GET', url: '/someUrl'}).
+        success(function(data, status, headers, config) {
+          // this callback will be called asynchronously
+          // when the response is available
+        }).
+        error(function(data, status, headers, config) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+      //$scope.state = !$scope.state;
+    };
+  }
+             );
